33b8ab25d1769c8101767259e59f842029e5b52c
Elnuget <cangulo009@outlook.es>
2023-08-31 03:34:08 -0500

    dbname2

--- Registro.php
+++ Registro.php
@@ -3,7 +3,7 @@
 // Conjuntos de credenciales para intentar
 $credentialSets = array(
     array("localhost", "root", "", "nutri"),
-    array("localhost", "gonzaloe_gonzaloe", "2+z0DZv#l95OYy", "gonzaloe_Prueba1")
+    array("www.gonzaloescudero.edu.ec", "gonzaloe_gonzaloe", "2+z0DZv#l95OYy", "gonzaloe_Prueba1")
     // Agrega más conjuntos de credenciales si es necesario
 );
 
--- dbconexion.php
+++ dbconexion.php
@@ -2,7 +2,7 @@
 $credentialSets = array(
     array("localhost", "root", "", "nutri"),
     // Conjunto 1
-    array("localhost", "gonzaloe_gonzaloe", "2+z0DZv#l95OYy", "gonzaloe_Prueba1") // Conjunto 2
+    array("www.gonzaloescudero.edu.ec", "gonzaloe_gonzaloe", "2+z0DZv#l95OYy", "gonzaloe_Prueba1") // Conjunto 2
 );
 $conn = null;
 
